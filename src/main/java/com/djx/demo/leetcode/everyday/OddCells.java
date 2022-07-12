93ecd2506ef659ee2d367c074baa08aac489c574
dongjingxi <dongjingqian@uniontech.com>
2022-07-12 21:22:33 +0800

    feat: 1252

---
+++ src/main/java/com/djx/demo/leetcode/everyday/OddCells.java
@@ -0,0 +1,30 @@
+package com.djx.demo.leetcode.everyday;
+
+/**
+ * @author djx
+ * @date 2022/7/12 下午9:18
+ */
+public class OddCells {
+
+
+    public int oddCells(int m, int n, int[][] indices) {
+
+
+        for (int i = 0; i < indices.length; i++) {
+            int row = indices[i][0];
+            int column = indices[i][1];
+        }
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] array = {};
+
+        OddCells oddCells = new OddCells();
+        oddCells.oddCells(3, 3, array);
+    }
+}
