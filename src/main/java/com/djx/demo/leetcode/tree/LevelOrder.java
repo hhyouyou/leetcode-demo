3f7b2b69b2807bf8dd5cd67488cd180e2ac177a6
dongjingxi <dongjingxiwin@outlook.com>
2023-02-08 23:40:00 +0800

    feat:  二叉树的层序遍历 复习bfs

--- src/main/java/com/djx/demo/leetcode/tree/LevelOrder.java
+++ src/main/java/com/djx/demo/leetcode/tree/LevelOrder.java
@@ -53,6 +53,36 @@ public class LevelOrder {
         return result;
     }
 
+    /**
+     * bfs
+     */
+    public static List<List<Integer>> levelOrder5(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+
 
     /**
      * [剑指 Offer 32 - I]从上到下打印二叉树
@@ -61,6 +91,33 @@ public class LevelOrder {
         List<List<Integer>> lists = levelOrder(root);
         return lists.stream().flatMapToInt(list -> list.stream().flatMapToInt(IntStream::of)).toArray();
     }
+
+    /**
+     * 方法二，广度优先搜索bfs
+     */
+    public static int[] levelOrder2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return new int[0];
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode pop = queue.pop();
+            result.add(pop.val);
+            if (pop.left != null) {
+                queue.add(pop.left);
+            }
+            if (pop.right != null) {
+                queue.add(pop.right);
+            }
+        }
+
+        return result.stream().mapToInt(x -> x).toArray();
+    }
+
 
     /**
      * [剑指 Offer 32 - III]从上到下打印二叉树 III
@@ -73,6 +130,43 @@ public class LevelOrder {
             if (i % 2 == 1) {
                 Collections.reverse(result.get(i));
             }
+        }
+
+        return result;
+    }
+
+    /**
+     * bfs
+     */
+    public static List<List<Integer>> levelOrder6(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean isLeft = true;
+
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> level = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (isLeft) {
+                    level.addLast(node.val);
+                } else {
+                    level.addFirst(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(level);
+            isLeft = !isLeft;
         }
 
         return result;
@@ -108,11 +202,21 @@ public class LevelOrder {
         List<List<Integer>> lists = levelOrder(root);
         System.out.println(lists.toString());
 
+        lists = levelOrder5(root);
+        System.out.println(lists.toString());
+
         int[] array = levelOrder1(root);
+        System.out.println(Arrays.toString(array));
+
+        array = levelOrder2(root);
         System.out.println(Arrays.toString(array));
 
         List<List<Integer>> lists1 = levelOrder3(root);
         System.out.println(lists1.toString());
+
+        lists1 = levelOrder6(root);
+        System.out.println(lists1.toString());
+
 
     }
 }
